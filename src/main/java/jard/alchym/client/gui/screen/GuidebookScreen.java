2b8d24f8680bf7b237c81632bd0964e8029bdbd7
Jared Barocsi <deyahruhd@gmail.com>
2021-01-03 01:47:12 -0800

    Finish pre-rendering setup code for the mod guide

--- src/main/java/jard/alchym/client/gui/screen/GuidebookScreen.java
+++ src/main/java/jard/alchym/client/gui/screen/GuidebookScreen.java
@@ -2,17 +2,51 @@ package jard.alchym.client.gui.screen;
 
 import jard.alchym.AlchymReference;
 import jard.alchym.api.book.BookPage;
+import jard.alchym.client.MatrixStackAccess;
+import jard.alchym.client.helper.BookHelper;
+import jard.alchym.helper.MathHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
+import net.minecraft.util.math.Matrix3f;
+import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Vec2f;
+
+import java.util.*;
 
 public class GuidebookScreen extends Screen {
-    private static final Identifier BOOK_TEXTURE [] = {
+    private static final Identifier [] BOOK_TEXTURE = {
         new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.1.png"),
-        new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.2.png"),
-        new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.3.png"),
+        //new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.2.png"),
+        //new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.3.png"),
         new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.4.png"),
+        //new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.5.png"),
+        //new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.6.png"),
+        //new Identifier (AlchymReference.MODID, "textures/gui/alchymic_reference.7.png"),
+    };
+
+    private static final List<Pair <Vec2f, Vec2f>> PAGE_COORDINATES  = new ArrayList<> ();
+    private static final List<Pair <Matrix4f, Matrix4f>> PAGE_SHEARS = new ArrayList<> ();
+
+    static {
+        // #1
+        PAGE_COORDINATES.add (new Pair<> (new Vec2f (77.f, 60.f), new Vec2f (336.f, 34.f)));
+        PAGE_SHEARS.add (new Pair<> (MathHelper.yShear (2.35f), MathHelper.IDENTITY_MATRIX));
+        // #2
+        //PAGE_COORDINATES.add (???);
+        // #3
+        //PAGE_COORDINATES.add (???);
+        // #4
+        PAGE_COORDINATES.add (new Pair<> (new Vec2f (72.f, 32.f), new Vec2f (344.f, 32.f)));
+        PAGE_SHEARS.add (new Pair<> (MathHelper.IDENTITY_MATRIX, MathHelper.IDENTITY_MATRIX));
+        // #5
+        //PAGE_COORDINATES.add (???);
+        // #6
+        //PAGE_COORDINATES.add (???);
+        // #7
+        //PAGE_COORDINATES.add (???);
     };
 
     private BookPage currentPage;
@@ -24,15 +58,40 @@ public class GuidebookScreen extends Screen {
     }
 
 
-    public void render (MatrixStack matrixStack, int i, int j, float f) {
-        this.renderBackground (matrixStack);
+    public void render (MatrixStack stack, int i, int j, float f) {
+        this.renderBackground (stack);
 
-        matrixStack.push ();
+        stack.push ();
 
-        matrixStack.scale (2, 2, 2);
+        stack.scale (2, 2, 2);
         this.client.getTextureManager ().bindTexture(BOOK_TEXTURE [0]);
-        this.drawTexture(matrixStack, (this.width - 320) / 4, 4, 0, 0, 160, 104);
+        this.drawTexture(stack, (this.width - 320) / 4, 4, 0, 0, 160, 104);
 
-        matrixStack.pop ();
+        stack.scale (0.25f, 0.25f, 0.25f);
+
+        Pair <Vec2f, Vec2f>       pageCoords = PAGE_COORDINATES.get (0);
+        Pair <Matrix4f, Matrix4f> pageShears = PAGE_SHEARS.get (0);
+
+        // Left page
+        stack.push ();
+        stack.translate ((this.width - 320) + pageCoords.getLeft ().x, pageCoords.getLeft ().y + 16.f, 0.f);
+        ((MatrixStackAccess) stack).multiply (pageShears.getLeft ());
+
+        // TODO: Render left page
+
+        stack.pop ();
+
+        // Right page
+        stack.push ();
+        stack.translate ((this.width - 320) + pageCoords.getRight ().x, pageCoords.getRight ().y + 16.f, 0.f);
+        ((MatrixStackAccess) stack).multiply (pageShears.getRight ());
+
+        // TODO: Render right page
+
+        stack.pop ();
+
+        // TODO: Implement all non-page rendering here (bookmarks, tabs, addons, etc)
+
+        stack.pop ();
     }
 }
